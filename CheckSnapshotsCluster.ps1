91a1bbe2b1f48c1ddfff3e5c22518a0562a864b6
lubian-spt <59941023+lubian-spt@users.noreply.github.com>
2021-03-12 04:44:47 -0300

    Add files via upload

---
+++ CheckSnapshotsCluster.ps1
@@ -0,0 +1,39 @@
+﻿Param(
+#Clustername
+[Parameter(Mandatory=$True)]
+[string]$cluster
+)
+
+
+$collection = @()
+
+$nodes = Get-ClusterNode -Cluster $cluster
+
+foreach($n in $nodes){
+
+    $vms = Get-VM -ComputerName $n.Name
+    
+        foreach($vm in $vms){
+        
+           $snapshot = Get-VMSnapshot -VMName $vm.Name -ComputerName $n.Name
+
+           foreach($snap in $snapshot){
+           
+                      if($snapshot -notlike $null){
+                      $itens = New-Object psobject
+                      $itens | Add-Member NoteProperty -Name "VM Name:" -Value $snap.VMName
+                      $itens | Add-Member NoteProperty -Name "Vm Node:" -Value $n
+                      $itens | Add-Member NoteProperty -Name "Snapshot name:" -Value $snap.Name
+
+                      $collection += $itens
+              }
+           
+           }
+
+
+
+                            }
+
+}
+
+$collection
